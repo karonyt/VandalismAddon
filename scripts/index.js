4489842636eb108c00551f85839e996180d8896b
karonyt <tomatotest34@gmaul.com>
2023-09-13 15:31:37 +0900

    update scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,87 +4,120 @@ system.runInterval(() => {
     world.getDimension(`overworld`).runCommandAsync(`gamerule tntexplodes true`)
     world.getAllPlayers().forEach(p => {
         if (p.hasTag(`nuker`) || hasMainItem(p, `karo:nuker`)) {
-            p.runCommandAsync(`fill ~20 ~5 ~20 ~-20 ~-5 ~-20 air destroy`)
+            p.runCommandAsync(`fill ~10 ~5 ~10 ~-10 ~-5 ~-10 air destroy`)
         }
-        if (p.hasTag(`title`) || hasMainItem(p, `karo:title`)) {
-            p.runCommandAsync(`title @a title §l§4§kaaa§r§l§c ${p.nameTag} §r§l§4§kaaa`)
+        if (p.hasTag(`chat`)) {
+            world.sendMessage(`§l§4HACKED BY ${p.nameTag}`)
         }
+    })
+})
+
+system.afterEvents.scriptEventReceive.subscribe((ev)=>{
+    if(ev.id === `karo:attack`) {
+        if(hasMainItem(ev.sourceEntity,`minecraft:stick`)) {
+            if(!ev.sourceEntity.getBlockFromViewDirection()) return
+            ev.sourceEntity.cooltime1 = 5
+            ev.sourceEntity.dimension.createExplosion(ev.sourceEntity.getBlockFromViewDirection().block.location,300)
+        }
+    }
+})
+
+system.runInterval(()=>{
+    world.getPlayers({ tags: [`falltnt`] }).forEach(p => {
         if (p.hasTag(`falltnt`) || hasMainItem(p, `karo:tnt`)) {
             p.runCommandAsync(`summon tnt ~ ~30 ~`)
         }
     })
-})
+},3)
+
+system.runInterval(() => {
+    world.getPlayers({ tags: [`title`] }).forEach(p => {
+        if (p.hasTag(`title`) || hasMainItem(p, `karo:title`)) {
+            p.runCommandAsync(`title @a title §l§4~+~§r§l§cHACKED BY ${p.nameTag} §r§l§4~+~`)
+        }
+    })
+},40)
 
 world.beforeEvents.chatSend.subscribe((ev) => {
     const { message, sender } = ev;
     if (!message.startsWith(`!`)) return
     ev.cancel = true
     const [command, sub, sub2] = message.split(` `)
-    switch (command) {
-        case `!title`: {
-            if (sender.hasTag(`title`)) {
-                sender.removeTag(`title`)
-            } else {
-                sender.addTag(`title`)
+    system.run(() => {
+        switch (command) {
+            case `!title`: {
+                if (sender.hasTag(`title`)) {
+                    sender.removeTag(`title`)
+                } else {
+                    sender.addTag(`title`)
+                }
+                break
             }
-            break
-        }
-        case `!tnt`: {
-            if (sender.hasTag(`falltnt`)) {
-                sender.removeTag(`falltnt`)
-            } else {
-                sender.addTag(`falltnt`)
+            case `!chat`: {
+                if (sender.hasTag(`chat`)) {
+                    sender.removeTag(`chat`)
+                } else {
+                    sender.addTag(`chat`)
+                }
+                break
             }
-            break
-        }
-        case `!bt`: {
-            // 点Aの座標
-            const A_x = sender.location.x;
-            const A_y = sender.location.y;
-            const A_z = sender.location.z;
+            case `!tnt`: {
+                if (sender.hasTag(`falltnt`)) {
+                    sender.removeTag(`falltnt`)
+                } else {
+                    sender.addTag(`falltnt`)
+                }
+                break
+            }
+            case `!bt`: {
+                // 点Aの座標
+                const A_x = sender.location.x;
+                const A_y = sender.location.y;
+                const A_z = sender.location.z;
 
-            // 円の半径
-            const r = Number(message.split(` `)[1]);
+                // 円の半径
+                const r = Number(message.split(` `)[1]);
 
-            // 度数からラジアンへの変換関数
-            function degreesToRadians(degrees) {
-                return (degrees * Math.PI) / 180;
-            }
+                // 度数からラジアンへの変換関数
+                function degreesToRadians(degrees) {
+                    return (degrees * Math.PI) / 180;
+                }
 
-            // 座標を取得する関数
-            function getCoordinates(latitudeDegrees, longitudeDegrees, centerX, centerY, centerZ, radius) {
-                const latitudeRadians = degreesToRadians(latitudeDegrees);
-                const longitudeRadians = degreesToRadians(longitudeDegrees);
-                const x = centerX + radius * Math.cos(longitudeRadians) * Math.sin(latitudeRadians);
-                const y = centerY + radius * Math.sin(longitudeRadians) * Math.sin(latitudeRadians);
-                const z = centerZ + radius * Math.cos(latitudeRadians);
-                return { x, y, z };
-            }
+                // 座標を取得する関数
+                function getCoordinates(latitudeDegrees, longitudeDegrees, centerX, centerY, centerZ, radius) {
+                    const latitudeRadians = degreesToRadians(latitudeDegrees);
+                    const longitudeRadians = degreesToRadians(longitudeDegrees);
+                    const x = centerX + radius * Math.cos(longitudeRadians) * Math.sin(latitudeRadians);
+                    const y = centerY + radius * Math.sin(longitudeRadians) * Math.sin(latitudeRadians);
+                    const z = centerZ + radius * Math.cos(latitudeRadians);
+                    return { x, y, z };
+                }
 
-            // 全ての座標を取得する
-            for (let latitude = -180; latitude <= 180; latitude++) {
-                system.run(() => {
-                    for (let longitude = 0; longitude < 180; longitude++) {
-                        const { x, y, z } = getCoordinates(latitude, longitude, A_x, A_y, A_z, r);
-                        world.getDimension(`overworld`).fillBlocks({ x: x, y: y, z: z }, { x: x, y: y, z: z }, BlockPermutation.resolve(`minecraft:tnt`))
-                    }
-                })
+                // 全ての座標を取得する
+                for (let latitude = -180; latitude <= 180; latitude++) {
+                    system.run(() => {
+                        for (let longitude = 0; longitude < 180; longitude++) {
+                            const { x, y, z } = getCoordinates(latitude, longitude, A_x, A_y, A_z, r);
+                            world.getDimension(`overworld`).fillBlocks({ x: x, y: y, z: z }, { x: x, y: y, z: z }, BlockPermutation.resolve(`minecraft:tnt`))
+                        }
+                    })
+                }
+                break
             }
-            break
-        }
-        case `!nuker`: {
-            if(sender.hasTag(`nuker`)) {
-                sender.removeTag(`nuker`)
-            } else {
-                sender.addTag(`nuker`)
+            case `!nuker`: {
+                if (sender.hasTag(`nuker`)) {
+                    sender.removeTag(`nuker`)
+                } else {
+                    sender.addTag(`nuker`)
+                }
+                break
             }
-            break
+            case `!getop`: {
+                sender.setOp(true)
+                break
+            }
         }
-        case `!getop`: {
-            sender.setOp(true)
-            break
-        }
-    }
+    })
 })
 
 /**
